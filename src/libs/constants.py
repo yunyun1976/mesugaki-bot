043552a301d9f517f92280a83d93207763fea387
yunyun1976 <137275844+yunyun1976@users.noreply.github.com>
2026-04-23 16:10:10 +0900

    constants.pyをリファクタリングした

--- src/libs/constants.py
+++ src/libs/constants.py
@@ -1,9 +1,9 @@
 import datetime
 import os
 
-# タイムゾーン設定♡
+# タイムゾーン設定
 JST = datetime.timezone(datetime.timedelta(hours=9))
 
-# ディレクトリ設定♡
+# ディレクトリ設定
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 DATA_DIR = os.path.join(BASE_DIR, 'data')
